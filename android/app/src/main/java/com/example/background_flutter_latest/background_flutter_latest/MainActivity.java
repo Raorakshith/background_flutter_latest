006d94aff5847ec5aeea7a3ea31a0fea130d618d
rakshith.rao@dataphilics.in <raorakshith990@gmail.com>
2021-07-09 15:39:58 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/background_flutter_latest/background_flutter_latest/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.background_flutter_latest.background_flutter_latest;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class MainActivity extends FlutterActivity {
+    private Intent forService;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //GeneratedPluginRegistrant.registerWith(this);
+        forService = new Intent(MainActivity.this,MyService.class);
+        new MethodChannel(getFlutterEngine().getDartExecutor(),"my.login_page.vitamind.messages").setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+            @Override
+            public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                if(call.method.equals("startService")){
+                    startservice();
+                    result.success("Service started");
+                }
+            }
+        });
+        if(!isMyServiceRunning(new MyService().getClass())){
+            startservice();
+        }
+    }
+    private void startservice(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            startForegroundService(forService);
+        }else {
+            startService(forService);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(forService);
+        Intent broadcastintent = new Intent();
+        broadcastintent.setAction("restartservice");
+        broadcastintent.setClass(this,Restarter.class);
+        this.sendBroadcast(broadcastintent);
+        super.onDestroy();
+    }
+    private  boolean isMyServiceRunning(Class<?> serviceClass){
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for(ActivityManager.RunningServiceInfo serviceInfo : manager.getRunningServices(Integer.MAX_VALUE)){
+            if(serviceClass.getName().equals(serviceInfo.service.getClassName())){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ android/app/src/main/java/com/example/background_flutter_latest/background_flutter_latest/Restarter.java
@@ -0,0 +1,20 @@
+package com.example.background_flutter_latest.background_flutter_latest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.widget.Toast;
+
+public class Restarter extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"Service restarted",Toast.LENGTH_SHORT).show();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            context.startForegroundService(new Intent(context,MyService.class));
+        }else {
+            context.startService(new Intent(context,MyService.class));
+        }
+    }
+}
